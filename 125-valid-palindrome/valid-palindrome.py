36f5612968b7e4429b65d87047ecd8fd09e29a8b
MEGHA CHOWDHURY <meghachowdhury.1176@gmail.com>
2026-09-03 16:04:16 +0200

    Time: 148 ms (21.20%) | Memory: 14.8 MB (5.74%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -8,9 +8,10 @@ class Solution(object):
         store = ""
         if s == " ":
             return True
-        for letter in s:
-            if letter.isalnum():
-                store += letter.lower()
+        else:
+            for letter in s:
+                if letter.isalnum():
+                    store += letter.lower()
         for i in range (len(store)-1):
             if  store[i] == store[len(store)-k]:
                 k += 1
