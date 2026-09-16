426dd5ef0cf000fc74170e64f8b02bcce2ef3b97
MEGHA CHOWDHURY <meghachowdhury.1176@gmail.com>
2026-09-17 00:25:44 +0200

    Time: 0 ms (100.00%) | Memory: 12.4 MB (55.55%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        num = str(n)
+        
+        check = set()
+        total = 0
+
+        while total not in check:
+            check.add(total)
+            total = 0
+            for i in num:
+                total += int(i)**2
+            if total == 1:
+                return True
+                
+            else:   
+                num = str(total)
+              
+        return False
+
+
+        
