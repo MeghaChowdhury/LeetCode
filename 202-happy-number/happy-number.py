6002e253e3ec88c2d87a4568b63b1375000ee610
MEGHA CHOWDHURY <meghachowdhury.1176@gmail.com>
2026-09-17 00:29:10 +0200

    Time: 0 ms (100.00%) | Memory: 12.2 MB (89.44%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -16,8 +16,8 @@ class Solution(object):
                 total += int(i)**2
             if total == 1:
                 return True
-            else:    
-                num = str(total)
+                
+            num = str(total)
               
         return False
 
